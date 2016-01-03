a1fa9eeb9e72c902a25f849bd06039d0c38d6bf3
Stephen Mathieson <me@stephenmathieson.com>
2016-01-02 20:39:34 -0800

    test: ensure we're in a browser env

--- test/index.js
+++ test/index.js
@@ -11,4 +11,8 @@ describe('smoke test', function () {
   it('should not explode', function () {
     assert.equal(2 + 2, 4);
   });
+
+  it('should have access to browser globals', function() {
+    assert.equal(typeof window, 'object');
+  });
 });
